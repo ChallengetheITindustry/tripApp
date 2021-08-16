d72cae0b5bbb803fb07456170aaed588d8fe3887
大西泰生 <t-z@oonishiyasuonoMacBook-Air.local>
2021-08-16 18:11:43 +0900

    main.dartにfirebaseのイニシャライザを記述

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tripapp/res/const.dart';
@@ -6,7 +7,9 @@ import 'package:tripapp/ui/login_page.dart';
 import 'package:tripapp/ui/login_signup_background.dart';
 import 'package:tripapp/ui/signup_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   WidgetsFlutterBinding.ensureInitialized();
   //向き指定
   SystemChrome.setPreferredOrientations([
