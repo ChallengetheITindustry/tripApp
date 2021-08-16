ed60fc338836e5b28b0de21e1bf59f993e7f83f6
大西泰生 <t-z@oonishiyasuonoMacBook-Air.local>
2021-08-16 18:32:06 +0900

    ログインと新規会員登録の定数を記述

--- lib/extension/extension.dart
+++ lib/extension/extension.dart
@@ -1,3 +1,10 @@
-String userName = "こんにちは"; // ユーザー名
+// ログイン
+String userName = ""; // ユーザー名
 String email = ""; // メールアドレス
-String password = "";  // パスワード
+String password = ""; // パスワード
+
+// 新規会員登録
+String newUserName = ""; // 登録に関する情報を表示
+String newEmail = ""; // 入力されたメールアドレス
+String newPassword = ""; // 入力されたパスワード
+bool pswd_OK = true;  // パスワードが有効な文字数を満たしているかどうか
