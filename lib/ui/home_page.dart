3b722f530d26109953d79012b3c3ff3c20630d23
大西泰生 <t-z@oonishiyasuonoMacBook-Air.local>
2021-08-17 12:40:45 +0900

    bottomNavigationのサンプルを表示

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,26 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:tripapp/ui/login_page.dart';
+import 'package:tripapp/ui/signup_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePage createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  int _currentIndex = 0;
+  final items = <BottomNavigationBarItem>[
+    BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      label: 'ホーム',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.timeline),
+      label: 'タイムライン',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.settings),
+      label: '設定',
+    ),
+  ];
+  final tabs = <Widget>[
+    SignupPageForm(),
+    LoginPageForm(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              child: Text('テストです'),
-            ),
-            FloatingActionButton(
-              child: Icon(Icons.add),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            )
-          ],
-        ),
+      body: Stack(
+        children: <Widget>[
+          IndexedStack(
+            index: _currentIndex,
+            children: tabs,
+          ),
+        ],
       ),
+      bottomNavigationBar: _buildBttomNavigator(context),
+    );
+  }
+
+  Widget _buildBttomNavigator(BuildContext context) {
+    return BottomNavigationBar(
+      items: items,
+      currentIndex: _currentIndex,
+      onTap: (index) {
+        if (_currentIndex != index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        }
+      },
     );
   }
 }
