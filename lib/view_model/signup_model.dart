dc79dde1cb9b66cc93b5e41437cecd343fbcfe5a
大西泰生 <t-z@oonishiyasuonoMacBook-Air.local>
2021-08-17 14:30:03 +0900

    新規会員登録したときに会員登録したときに正しいメールアドレスか確認するための処理を実装

--- lib/view_model/signup_model.dart
+++ lib/view_model/signup_model.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignUpModel extends ChangeNotifier {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final _auth = FirebaseAuth.instance;
 
   // 新規会員登録
   String newUserName = ""; // 登録に関する情報を表示
@@ -10,6 +10,12 @@ class SignUpModel extends ChangeNotifier {
   String newPassword = ""; // 入力されたパスワード
   // ignore: non_constant_identifier_names
   bool pswd_OK = true; // パスワードが有効な文字数を満たしているかどうか
+
+// FirebaseUser has been changed to User
+// AuthResult has been changed to UserCredential
+
+  late UserCredential _result;
+  late User _user;
 
   Future signUp() async {
     if (newEmail.isEmpty) {
@@ -20,11 +26,19 @@ class SignUpModel extends ChangeNotifier {
       throw 'パスワードをを入力してください';
     }
 
-    // ignore: unused_local_variable
-    final User? user = (await _auth.createUserWithEmailAndPassword(
-      email: newEmail,
-      password: newPassword,
-    ))
-        .user;
+    try {
+      // バリデーション後のメールアドレスとパスワードでアカウント登録
+      _result = await _auth.createUserWithEmailAndPassword(
+        email: newEmail,
+        password: newPassword,
+      );
+
+      // 登録成功
+      _user = _result.user!; // 登録したユーザー情報
+      _user.sendEmailVerification(); // Email確認のメールを送信
+
+    } catch (error) {
+      throw error;
+    }
   }
 }
