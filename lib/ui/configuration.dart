5e6b85b2c677d0e7c3e59b1953871642073e217a
大西泰生 <t-z@oonishiyasuonoMacBook-Air.local>
2021-08-17 15:08:03 +0900

    configurationのサンプルコードを記述

--- lib/ui/configuration.dart
+++ lib/ui/configuration.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tripapp/res/const.dart';
 
 class ConfigurationPage extends StatelessWidget {
   @override
@@ -10,28 +11,170 @@ class ConfigurationPage extends StatelessWidget {
           padding: EdgeInsets.all(36.0),
           shrinkWrap: true,
           children: [
-            Container(
-              color: Colors.red,
-              child: Text(
-                "ジョナサン・ジョースター",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 25.0),
+            SizedBox(
+              height: 100,
+              child: Card(
+                child: ListTile(
+                  leading: Icon(Icons.exit_to_app),
+                  title: Text(
+                    "ログアウト",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  trailing: Icon(Icons.more_vert),
+                  contentPadding: EdgeInsets.all(10.0),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
               ),
             ),
-            Container(
-              color: Colors.green,
-              child: Text(
-                "ジョセフ・ジョースター",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 25.0),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with subTitle"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text("This is subtitle."),
+                onTap: () {},
               ),
             ),
-            Container(
-              color: Colors.blue,
-              child: Text(
-                "空条承太郎",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 25.0),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with long subTitle"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text(
+                    "This is subtitle. Subtitle is very long and use three lines"),
+                onTap: () {},
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with isThreeLine true"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text(
+                    "This is subtitle. Subtitle is very long and use three lines."),
+                onTap: () {},
+                isThreeLine: true,
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with dense true"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text("This is subtitle."),
+                onTap: () {},
+                dense: true,
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with enabled false"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text("This is subtitle."),
+                enabled: false,
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with selected true"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text("This is subtitle."),
+                selected: true,
+                onTap: () {},
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with contentPadding all 10.0"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text("This is subtitle."),
+                onTap: () {},
+                contentPadding: EdgeInsets.all(10.0),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with contentPadding all 10.0"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text("This is subtitle."),
+                onTap: () {},
+                contentPadding: EdgeInsets.all(10.0),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with contentPadding all 10.0"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text("This is subtitle."),
+                onTap: () {},
+                contentPadding: EdgeInsets.all(10.0),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with contentPadding all 10.0"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text("This is subtitle."),
+                onTap: () {},
+                contentPadding: EdgeInsets.all(10.0),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with contentPadding all 10.0"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text("This is subtitle."),
+                onTap: () {},
+                contentPadding: EdgeInsets.all(10.0),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with contentPadding all 10.0"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text("This is subtitle."),
+                onTap: () {},
+                contentPadding: EdgeInsets.all(10.0),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with contentPadding all 10.0"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text("This is subtitle."),
+                onTap: () {},
+                contentPadding: EdgeInsets.all(10.0),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with contentPadding all 10.0"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text("This is subtitle."),
+                onTap: () {},
+                contentPadding: EdgeInsets.all(10.0),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text("ListTile with contentPadding all 10.0"),
+                trailing: Icon(Icons.more_vert),
+                subtitle: Text("This is subtitle."),
+                onTap: () {},
+                contentPadding: EdgeInsets.all(10.0),
               ),
             ),
           ],
