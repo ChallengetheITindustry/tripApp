a41dbe8b747c2f6bc3c0bb275e0c57ba98f5772b
大西泰生 <t-z@oonishiyasuonoMacBook-Air.local>
2021-08-22 12:33:40 +0900

    dots_indicatorを実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -21,13 +22,25 @@ void main() async {
 
 class MyApp extends StatefulWidget {
   @override
-  _MyApp createState() => _MyApp();
+  MyAppPage createState() => MyAppPage();
 }
 
-class _MyApp extends State {
-  final PageController controller = PageController(initialPage: 0);
+class MyAppPage extends State {
+  final PageController _controller = PageController(initialPage: 0);
+
+  // ignore: unused_field
+  double _currentPage = 0;
+
+  final _pageLength = 2;
+  // main.dartファイルの_currentPageを参照し、これが変わることによってアクティブなdotが変更される
+
+  List _pages = [LoginPageForm(), SignupPageForm()];
   @override
   Widget build(BuildContext context) {
+    // 幅
+    double width = MediaQuery.of(context).size.width;
+    // 高さ
+    double height = MediaQuery.of(context).size.height;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'tripApp',
@@ -39,12 +52,38 @@ class _MyApp extends State {
           elevation: 0.0,
         ),
         body: Stack(children: [
-          Dots(),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  child: DotsIndicator(
+                    dotsCount: _pageLength,
+                    position: _currentPage,
+                    decorator: DotsDecorator(
+                      activeColor: primaryColor,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: width,
+                  height: height * 0.1,
+                ),
+              ],
+            ),
+          ),
           Background(),
-          PageView(controller: controller, children: [
-            LoginPageForm(),
-            SignupPageForm(),
-          ]),
+          PageView.builder(
+              itemCount: _pages.length,
+              controller: _controller,
+              onPageChanged: (int index) {
+                setState(() {
+                  _currentPage = index.toDouble();
+                });
+              },
+              itemBuilder: (context, index) {
+                return _pages[index];
+              }),
         ]),
       ),
     );
--- lib/ui/dots.dart
+++ lib/ui/dots.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:dots_indicator/dots_indicator.dart';
+import 'package:tripapp/main.dart';
 import 'package:tripapp/res/const.dart';
 
 // ignore: must_be_immutable
 class Dots extends StatelessWidget {
+  // // 下記でインスタンス化(実体化)する
+  // MyAppPage myAppPage = MyAppPage();
   final _pageLength = 2;
+  // main.dartファイルの_currentPageを参照し、これが変わることによってアクティブなdotが変更される
   double _currentPosition = 0;
+
   @override
   Widget build(BuildContext context) {
     // 幅
