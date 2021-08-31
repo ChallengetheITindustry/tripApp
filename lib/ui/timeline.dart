5b67c12ea7796eced73fc0a97972e6f544f4a433
大西泰生 <t-z@oonishiyasuonoMacBook-Air.local>
2021-09-01 00:23:41 +0900

    タイムラインのスタイルを調整

--- lib/ui/timeline.dart
+++ lib/ui/timeline.dart
@@ -1,8 +1,11 @@
 import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:tripapp/config/config.dart';
 import 'package:tripapp/res/const.dart';
 import 'package:video_player/video_player.dart';
+
+import 'map.dart';
 
 class TimeLinePage extends StatefulWidget {
   @override
@@ -13,28 +16,115 @@ class TimeLinePage extends StatefulWidget {
 class _TimeLinePage extends State {
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
         backgroundColor: timelineBackground,
         body: Stack(
-          alignment: Alignment.center,
           children: [
             Positioned(
-              top: 100,
-              child: Text(
-                '旅を共有しよう',
-                style: GoogleFonts.lobster(
+              top: SizeConfig.blockSizeVertical * 8,
+              right: SizeConfig.blockSizeHorizontal * 80,
+              child: Container(
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.volume_up_outlined,
+                    size: 40,
                     color: Colors.white,
-                    fontSize: 20,
-                    textStyle: Theme.of(context).textTheme.headline4),
+                  ),
+                ),
               ),
             ),
+            Container(child: Image.asset('assets/images/moon.png')),
+            Positioned(
+              top: SizeConfig.blockSizeVertical * 20,
+              left: 50,
+              right: 50,
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black12,
+                            spreadRadius: 1.0,
+                            offset: Offset(15, 15))
+                      ],
+                    ),
+                    child: Text(
+                      '旅を共有しよう',
+                      style: GoogleFonts.lobster(
+                          color: Colors.white,
+                          fontSize: 20,
+                          textStyle: Theme.of(context).textTheme.headline4),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // ListView.builder(
+            //   itemCount: list.length,
+            //   itemBuilder: (BuildContext context, int index) {
+            //     return Container(
+            //       height: SizeConfig.blockSizeVertical * 8,
+            //       child: Card(child: ListTile(leading: Text(list[index]))),
+            //     );
+            //   },
+            // ),
+
             Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                // Image.asset(
-                //   'assets/images/kirakira.png',
-                // ),
                 Image.asset('assets/images/tamariba.png'),
+                Opacity(
+                  opacity: 0.3,
+                  child: Container(
+                      width: SizeConfig.blockSizeHorizontal * 60,
+                      height: SizeConfig.blockSizeVertical * 8,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 10.0,
+                              spreadRadius: 5.0,
+                              offset: Offset(10, 10))
+                        ],
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.chat_bubble_outline,
+                                size: 30,
+                              )),
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.favorite_border_outlined,
+                                size: 30,
+                              )),
+                          IconButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => MapPage()),
+                                );
+                              },
+                              icon: Icon(
+                                Icons.map_outlined,
+                                size: 30,
+                              )),
+                        ],
+                      )),
+                ),
+                SizedBox(
+                  height: SizeConfig.blockSizeVertical * 7,
+                )
               ],
             ),
           ],
