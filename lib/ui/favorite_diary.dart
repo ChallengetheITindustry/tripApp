0a2ac25421dec3ae7313b425e648e6b39f123538
大西泰生 <t-z@oonishiyasuonoMacBook-Air.local>
2021-09-11 11:58:04 +0900

    favorite_diary.dartファイルにProviderを利用するための型を記述

--- lib/ui/favorite_diary.dart
+++ lib/ui/favorite_diary.dart
@@ -1,58 +1,65 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tripapp/config/config.dart';
+import 'package:tripapp/view_model/favorite_diary_model.dart';
 
 class FavoriteDiary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-        onPressed: () {
-          showModalBottomSheet(
-              //モーダルの背景の色、透過
-              backgroundColor: Colors.transparent,
-              //ドラッグ可能にする（高さもハーフサイズからフルサイズになる様子）
-              isScrollControlled: true,
-              context: context,
-              builder: (BuildContext context) {
-                return Stack(
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+    return ChangeNotifierProvider<FavoriteDiaryModel>(
+      create: (_) => FavoriteDiaryModel(),
+      child: Consumer<FavoriteDiaryModel>(builder: (context, model, child) {
+        return IconButton(
+            onPressed: () {
+              showModalBottomSheet(
+                  //モーダルの背景の色、透過
+                  backgroundColor: Colors.transparent,
+                  //ドラッグ可能にする（高さもハーフサイズからフルサイズになる様子）
+                  isScrollControlled: true,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Stack(
                       children: [
-                        Center(
-                          child: Container(
-                            width: SizeConfig.screenWidth * 0.8,
-                            decoration: BoxDecoration(
-                              border: const Border(
-                                bottom: const BorderSide(
-                                  color: Colors.white,
-                                  width: 1,
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Center(
+                              child: Container(
+                                width: SizeConfig.screenWidth * 0.8,
+                                decoration: BoxDecoration(
+                                  border: const Border(
+                                    bottom: const BorderSide(
+                                      color: Colors.white,
+                                      width: 1,
+                                    ),
+                                  ),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(bottom: 8.0),
+                                  child: Text(
+                                    'いいね一覧',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
-                            child: Padding(
-                              padding: const EdgeInsets.only(bottom: 8.0),
-                              child: Text(
-                                'いいね一覧',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ),
+                          ],
                         ),
                       ],
-                    ),
-                  ],
-                );
-              });
-        },
-        icon: Icon(
-          Icons.favorite,
-          size: 30,
-        ));
+                    );
+                  });
+            },
+            icon: Icon(
+              Icons.favorite,
+              size: 30,
+            ));
+      }),
+    );
   }
 }
---
+++ lib/view_model/favorite_diary_model.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+class FavoriteDiaryModel extends ChangeNotifier {}
